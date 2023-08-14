83c4b08572ff52e816694b408828ad698f15a4fc
codewithbersh <121822380+codewithbersh@users.noreply.github.com>
2023-08-14 20:28:19 +0300

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,11 +25,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-c50f%++g)k=3$!n8!dgvzzlhng1+r7xce-!jd=_8+$4xs$qmmb"
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-# DEBUG = False
+# DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "teamspace-production.up.railway.app",
@@ -57,7 +57,6 @@ INSTALLED_APPS = [
     "allauth",
     "allauth.account",
     "allauth.socialaccount",
-    # Add social providers
     "allauth.socialaccount.providers.google",
     "accounts",
     "corsheaders",
@@ -154,27 +153,27 @@ WSGI_APPLICATION = "backend.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": env("PG_DATABASE"),
-#         "USER": env("PG_USER"),
-#         "PASSWORD": env("PG_PASSWORD"),
-#         "HOST": env("PG_HOST"),
-#         "PORT": env("PG_PORT"),
-#     }
-# }
-
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "teamspace",
-        "USER": "bersh",
-        "PASSWORD": "124398",
-        "HOST": "localhost",
-        "PORT": "5432",
+        "NAME": env("PG_DATABASE"),
+        "USER": env("PG_USER"),
+        "PASSWORD": env("PG_PASSWORD"),
+        "HOST": env("PG_HOST"),
+        "PORT": env("PG_PORT"),
     }
 }
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "teamspace",
+#         "USER": "bersh",
+#         "PASSWORD": "124398",
+#         "HOST": "localhost",
+#         "PORT": "5432",
+#     }
+# }
 
 
 # Password validation
